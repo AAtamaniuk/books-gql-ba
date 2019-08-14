595812fad864fe805810240321e085cfa2cf0ed9
Aleksandr Atamaniuk <atamaniuk.dev@gmail.com>
2019-08-14 14:01:49 +0300

    feat(app.js package.json): add database connection
    add moongoose to dependencies
     add dotenv to dependencies
     add database settings and connection

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+console.log(process.env.DB_HOST);
+
+mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true }).catch(err => {
+  console.log("Connection to database error\n" + err);
+});
+
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 app.use(
   "/graphql",
